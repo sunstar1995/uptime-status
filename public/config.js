24a3db0403a59f2e13b061d5fa20275d5710c23f
liuxin <sunstarlx@163.com>
2023-07-09 02:10:40 +0800

    修改配置

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '神经蛙的站点监控',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2209895-70365cf1931e348191194989',  // 神经蛙线路一
   ],
 
   // 日志天数
@@ -22,15 +19,27 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://home.sjava.cn/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: '神经蛙线路一',
+      url: 'https://hexo.sjava.cn/'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: '神经蛙线路二',
+      url: 'https://hexoblog.sjava.cn/'
+    },
+    {
+      text: '芈亓的Blog',
+      url: 'https://lx95.top/'
+    },
+    {
+      text: 'ChatGPT',
+      url: 'https://chatgpt.sjava.cn/'
+    },
+    {
+      text: '知识速查',
+      url: 'https://quick.sjava.cn/'
     },
   ],
 };
